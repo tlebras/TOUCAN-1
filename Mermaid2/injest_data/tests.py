e8491ecd4ed56660b12597ff154f370a7104fb99
mermaid2 <jean-gil.langlois@orange.fr>
2013-09-11 15:36:18 +0100

    Update tests.py

--- Mermaid2/injest_data/tests.py
+++ Mermaid2/injest_data/tests.py
@@ -11,7 +11,7 @@ from injest_data.injest import *
 class InjestToolsTest(TestCase):
 
     def setUp(self):
-        self.sampleData = open("/home/langlois/Documents/django/Mermaid2/Mermaid2/injest_data/extraction_Test_.csv", "r")
+        self.sampleData = open("/home/mermaid2/mermaid2/Mermaid2/injest_data/extraction_Test_.csv", "r")
         self.instrument = Instrument.objects.get_or_create(name='Unknown')[0]
         
     def test_read_file(self):    
