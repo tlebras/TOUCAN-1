4123f4427d2a3d9ca0f8cda2ecd89c549a596909
Clare O'Neill <CONeill@argans.co.uk>
2014-06-04 16:18:30 +0100

    Extract data fields
    Start the radiometric drift routine, by reading
    data fields from the two JSON object inputs and
    storing in dictionaries.
    
    Perform a check to make sure that both datasets
    are for the same region of interest (might want
    to add other checks here later?)
    
    Also add unit tests for new methods

--- toucan/tools/librad_drift.py
+++ toucan/tools/librad_drift.py
@@ -1,5 +1,27 @@
+import numpy as np
+import datetime
+
 from libbase import ToolBase
 from libquerydb import Querydb
+import libtools
+
+
+def main():
+
+    #  Query the database for matching files
+    Q = Querydb()
+    search = {'site': 'Libya4',
+              'sensor': 'meris',
+              'end_date': '2006-12-31',
+              'order_by': 'time',
+              }
+    reference = Q.get_images(search)
+
+    search['sensor'] = 'aatsr'
+    target = Q.get_images(search)
+
+    drift = RadiometricDrift()
+    drift.run(reference, target)
 
 
 class RadiometricDrift(ToolBase):
@@ -8,6 +30,19 @@ class RadiometricDrift(ToolBase):
     """
     def run(self, reference, target):
         # User specifies reference sensor and target sensor, date range, and site
+        jsonresults = {'reference': reference,
+                       'target': target
+        }
+
+        # -------------------------------
+        # Extract fields we need
+        # -------------------------------
+        data = {}
+        for sensor in ('reference', 'target'):
+            data[sensor] = self.extract_fields(jsonresults[sensor])
+
+        # Check data are ok
+        self.check_fields(data['reference'], data['target'])
 
         # Doublets are found using angular matching criteria
 
@@ -17,3 +52,38 @@ class RadiometricDrift(ToolBase):
 
         # Text file saved
         pass
+
+    @staticmethod
+    def extract_fields(jsonresults):
+        """
+        Extract fields that we need from the JSON object
+        and return in a nicer format
+        """
+        files = np.array([result['archive_location'] for result in jsonresults])
+        dates = np.array([datetime.datetime.strptime(result['time'], '%Y-%m-%dT%H:%M:%S.%f') for result in jsonresults])
+        angles = libtools.get_angles(jsonresults)
+        sun_zenith, sensor_zenith, relative_azimuth = angles
+        instrument = jsonresults[0]['instrument']['name']
+        region = jsonresults[0]['region']['region']
+
+        # Create dictionary holding all fields
+        data = {'files': files,
+                'dates': dates,
+                'SZA': sun_zenith,
+                'VZA': sensor_zenith,
+                'RAA': relative_azimuth,
+                'instrument': instrument,
+                'region': region,
+                }
+        return data
+
+    @staticmethod
+    def check_fields(reference, target):
+        """
+        Check that target and reference regions match
+        """
+        if reference['region'] != target['region']:
+            raise IOError("ERROR: Target and reference sensor regions do not match")
+
+if __name__ == '__main__':
+   main()
--- toucan/tools/tests/test_librad_drift.py
+++ toucan/tools/tests/test_librad_drift.py
@@ -1,8 +1,45 @@
 from django.test import TestCase
 from tools import librad_drift
-
+from mock import *
+import datetime
 
 class RadiometricDriftTests(TestCase):
     """
     Test the radiometric drift library
     """
+    def test_extract_fields(self):
+        """
+        Test the extraction of fields from JSON object
+        """
+        # Set up test dictionary, and the result we expect to get back
+        blah = 'blah'   # Arbitrary string
+        date = datetime.datetime(2000, 1, 1)
+        angle = 0
+        testjson = ({'archive_location': blah,
+                     'time': date.strftime('%Y-%m-%dT%H:%M:%S.%f'),
+                     'instrument': {'name':blah},
+                     'region': {'region':blah},
+                    }, )  # Needs to be a list
+
+        expected_data = {'files': blah,
+                         'dates': date,
+                         'SZA': angle,
+                         'VZA': angle,
+                         'RAA': angle,
+                         'instrument': blah,
+                         'region': blah,
+                         }
+
+        # Mock libtools.get_angles as that is tested elsewhere
+        with patch('tools.libtools.get_angles') as mock:
+            mock.return_value = (angle, angle, angle)
+            data = librad_drift.RadiometricDrift.extract_fields(testjson)
+        self.assertDictEqual(data, expected_data)
+
+    def test_region_check(self):
+        """
+        Check that exception is raised if target and reference regions don't match
+        """
+        reference = {'region': 'reference'}
+        target = {'region': 'target'}
+        self.assertRaises(IOError, librad_drift.RadiometricDrift.check_fields, reference, target)
